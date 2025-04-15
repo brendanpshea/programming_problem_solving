5cbfdbc3d39be2616afe3ba8d93ef264883e4d0e
Brendan Shea <39303594+brendanpshea@users.noreply.github.com>
2025-04-15 14:38:05 +0000

    start for fall 2025

--- java/ch09/BadIdea.java
+++
@@ -1,8 +0,0 @@
-public class BadIdea{
-    public static void main(String[] args){
-      SuperPet garfield = new SuperPet("Garfield", "Eats Lasagna");
-  
-      System.out.println(garfield.getName());
-    }
-  
-  }
--- java/ch09/PetFoodTest.java
+++
@@ -1,6 +0,0 @@
-public class PetFoodTest {
-    public static void main(String[] args) {
-        PetFood food = new PetFood("HappyPet", "Dry", 15.5);
-        System.out.println(food);
-    }
-}
